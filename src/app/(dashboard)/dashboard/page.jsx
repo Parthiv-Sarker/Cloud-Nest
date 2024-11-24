f0b6fda7322b6e6a3dfaa22c18231772ada36a9d
Parthiv-Sarker <parthivsdevelopment@gmail.com>
2024-11-24 13:52:58 +0530

    Create dashboard layout.

--- src/app/(dashboard)/dashboard/page.jsx
+++ src/app/(dashboard)/dashboard/page.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+"use client";
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+import React from "react";
+import authService from "@/appwrite/authService";
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/hooks/use-toast";
 
-export default page
+const Page = () => {
+  const { toast } = useToast();
+	const onLogout = async () => {
+    
+		try {
+			await authService.logout();
+			toast({
+				variant: "success",
+				title: "Logged out successfully.",
+			});
+		} catch (error) {
+			toast({
+				variant: "destructive",
+				title: "Logout failed.",
+				description: error.message || "An unexpected error occurred.",
+			});
+			console.error("Logout error:", error);
+		}
+	};
+
+	return (
+		<div className="flex justify-center items-center h-screen">
+			<Button onClick={onLogout}>Log Out</Button>
+		</div>
+	);
+};
+
+export default Page;
